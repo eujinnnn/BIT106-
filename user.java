0b7c6f63b675d0de06fcc8a0cbb58958f9d011df
eujinnnn <138985217+eujinnnn@users.noreply.github.com>
2023-10-24 17:08:07 +0800

    Update and rename test.html to user.java

---
+++ user.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class user {
+    public static void editUser (){
+        
+    }
+    
+    public static void viewUser (){
+        
+    }
+}
